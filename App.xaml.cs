6a07dbebf46b4c1ca45ea506b13366d162dcea00
kabaidani <kabaidani5@gmail.com>
2022-02-18 20:26:47 +0100

    Added view elements, relatie paths problem is fixed

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using PokerGame.view;
+
+namespace PokerGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainWindow _mainWindow;
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            _mainWindow = new MainWindow();
+            _mainWindow.Show();
+        }
+    }
+}
